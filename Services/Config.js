9fd5d0e5fdc74c15dbceb961d89ceb1f92d67495
abdallah-hatem <ahkortam@gmail.com>
2023-09-18 03:04:22 +0300

    added new backend url

--- Services/Config.js
+++ Services/Config.js
@@ -1,2 +1,2 @@
 export const ApiBaseUrl2 = "http://localhost:8000/";
-export const ApiBaseUrl = "https://ecommerce-back-end-alpha.vercel.app/";
+export const ApiBaseUrl = "https://ecommerce-back-end-seven.vercel.app/";
